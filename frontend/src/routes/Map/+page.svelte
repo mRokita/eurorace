7aa08cb972d623947668073faaf0636092ede3a4
korniszon99 <kornel.musial@gmail.com>
2025-03-02 15:01:07 +0100

    feat: added Marker

--- frontend/src/routes/Map/+page.svelte
+++ frontend/src/routes/Map/+page.svelte
@@ -2,9 +2,18 @@
 	import '../../app.css';
 	import type { LatLngExpression } from 'leaflet';
 	import Leaflet from '$lib/Leaflet.svelte'; // Ensure this path is correct
+	import Marker from '$lib/Marker.svelte'; // Ensure this path is correct
+
 	const initialView: LatLngExpression = [52.25, 20.9];
+	const markerLocations: Array<LatLngExpression> = [
+		[52.25, 20.9],
+	];
 </script>
 
 <div class="w-full h-screen">
-	<Leaflet view={initialView} zoom ={13}/>
+	<Leaflet view={initialView} zoom ={13}>
+		{#each markerLocations as latLng}
+			<Marker {latLng} width={40} height={40}/>
+		{/each}
+	</Leaflet>
 </div>
